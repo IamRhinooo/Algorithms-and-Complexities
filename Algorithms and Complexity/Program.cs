c55b255ac3f759ebb0be6feeffdad053fb3f85c2
Rhino <reuben.hover@gmail.com>
2025-03-24 19:34:32 +0000

    New classes + sort

--- Algorithms and Complexity/Program.cs
+++ Algorithms and Complexity/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_Complexities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The share 1 stocks are:");
+            foreach (var line in Stocks.share1)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("The share 2 stocks are:");
+            foreach (var line in Stocks.share2)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("The share 3 stocks are:");
+            foreach (var line in Stocks.share3)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Algorithms and Complexity/Stocks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_Complexities
+{
+    class Stocks
+    {
+        public static string[] share1;
+        public static string[] share2;
+        public static string[] share3;
+
+        static Stocks()
+        {
+            share1 = File.ReadAllLines("share_1_256.txt");
+            share2 = File.ReadAllLines("share_2_256.txt");
+            share3 = File.ReadAllLines("share_3_256.txt");
+
+            Convert.ToInt32(share1[0]);
+            Convert.ToInt32(share2[0]);
+            Convert.ToInt32(share3[0]);
+        }
+    }
+}
